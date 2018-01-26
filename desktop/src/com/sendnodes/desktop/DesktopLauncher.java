ad1dac43d4e162dec864e0ba439f2dc4ada298df
moorcroft s. (sm18g15) <sm18g15@soton.ac.uk>
2018-01-26 20:16:26 +0000

    initial commit

---
+++ desktop/src/com/sendnodes/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.sendnodes.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.sendnodes.Core;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Core(), config);
+	}
+}
