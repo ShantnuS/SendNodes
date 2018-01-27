e90f5ef54fd050f423228c4a51e78934937ae19d
hercu <hercu@10.9.168.176>
2018-01-27 19:01:17 +0000

    Added music files Creating a audio manager class structre

--- core/src/soundengine/SoundManager.java
+++ core/src/soundengine/SoundManager.java
@@ -8,11 +8,9 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.utils.Queue;
 
-public class SoundManager {
+public class SoundManager extends AudioManager{
 	
-	public float volume = 1.0f;
-	public boolean muted = false;
-	
+	private static String path = "Sounds/";
 	public static enum SOUNDS{
 		BACK,
 		EXCHANGE,
@@ -20,44 +18,38 @@ public class SoundManager {
 		SCROLL,
 		START_GAME,
 		CLICK,
-	}
-	
+	}	
 	private static String[] soundPaths = {
-		"back.mp3",
-		"exchange.mp3",
-		"hover.mp3",
-		"scroll.mp3",
-		"startgame.mp3",
-		"click.mp3"
+		path + "back.mp3",
+		path + "exchange.mp3",
+		path + "hover.mp3",
+		path + "scroll.mp3",
+		path + "startgame.mp3",
+		path + "click.mp3"
 	};
-	
-	public ArrayList<Sound> sounds = new ArrayList<Sound>();
 	
 	public SoundManager()
 	{
 		for(String s : soundPaths)
 			sounds.add(Gdx.audio.newSound(Gdx.files.internal(s)));		
 	}
-	
-	public void playSound(int NAME)
+
+	@Override
+	public void playSound(final int NAME)
 	{
 		if(!muted)
-			sounds.get(NAME).play(volume);
+		{
+			new Thread(new Runnable() {
+
+				@Override
+				public void run() {
+					sounds.get(NAME).play(volume);					
+				}
+				
+			}).start();			
+		}
 	}
 	
-	public float setVolume(float volume)
-	{
-		return this.volume = volume;
-	}
 	
-	public boolean toggleMute()
-	{
-		return muted = !muted;
-	}
 	
-	private void test()
-	{
-		for(Sound s : sounds)
-		 s.play();
-	}
 }
