299a403fbdbd1a032ea44d26ff6ee3ca84f9da95
sm18g15 <sm18g15@m17.clients.soton.ac.uk>
2018-01-27 16:36:50 +0000

    clicking

--- core/src/com/sendnodes/GameController.java
+++ core/src/com/sendnodes/GameController.java
@@ -2,17 +2,21 @@ package com.sendnodes;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.sendnodes.entities.EntityManager;
+import com.sendnodes.ui.UIManager;
 
 public class GameController {
 	
 	private EntityManager entityManager;
+	private UIManager uiManager;
 	
 	public GameController(int map_size) {
 		entityManager = new EntityManager(map_size);
+		uiManager = new UIManager(entityManager);
 	}
 	
 	public void update() {
 		entityManager.update();
+		uiManager.update();
 	}
 	
 	public void render(SpriteBatch batch){
