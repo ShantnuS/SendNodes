589da57d0e1581b2a4e7f705ad96bed334acb88b
jt10g15 <jt10g15@m20.clients.soton.ac.uk>
2018-01-27 15:41:04 +0000

    Added cost to power ups and per-turn payment method

--- core/src/powerups/PowerUp.java
+++ core/src/powerups/PowerUp.java
@@ -4,6 +4,8 @@ public abstract class PowerUp implements PUInterface {
 
 	String name; 
 	String description;
+	final int max_cost = 20; // cost
+	int curr_cost = 20; // the cost at the moment
 	
 	public String getName() {
 		return name;
@@ -19,6 +21,23 @@ public abstract class PowerUp implements PUInterface {
 
 	public void setDescription(String description) {
 		this.description = description;
+	}
+	
+	// called each round
+	// takes the points applied to power up
+	// returns whether or not the power up cost has been paid yet or not
+	public boolean payPowerUpCost(int points) {
+		curr_cost -= points;
+		if (curr_cost <= 0) {
+			curr_cost = max_cost;
+			return true;
+		}
+		return false;
+	}
+	
+	// cost of this power up at the moment
+	public int getCurrentCost() {
+		return curr_cost;
 	}
 
 	public PowerUp(String name, String description) {
