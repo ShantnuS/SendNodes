d13538d9a7c31d1b15a0f6584230755e8f1ec876
sm18g15 <sm18g15@m17.clients.soton.ac.uk>
2018-01-27 13:02:28 +0000

    attack target updated

--- core/src/com/sendnodes/entities/Player.java
+++ core/src/com/sendnodes/entities/Player.java
@@ -26,17 +26,25 @@ public class Player {
 	}
 	
 	private void attackTargets(){
+		int remainingIp = ip;
 		for (Attack target:getTargets()) {
-			//--PLACE HOLDER-- ATTACKS NEED OWN OBJECT TO GET THIS VALUE
 			int amountToDamageTarget = target.getDamage();
+			if(remainingIp <= 0){
+				return;
+			}
 			for(Connection conn:target.getTarget().getConnections()) {
+				if(amountToDamageTarget>remainingIp){
+					amountToDamageTarget = remainingIp;
+				}
 				if(conn.getOtherNode(target.getTarget()).getOwner() == this) {
 					if(amountToDamageTarget<=conn.getBandwidth()) {
 						target.getTarget().adjustHealth(amountToDamageTarget, this);
 						amountToDamageTarget-=amountToDamageTarget;
+						remainingIp-=amountToDamageTarget;
 					}else {
 						target.getTarget().adjustHealth(conn.getBandwidth(), this);
 						amountToDamageTarget-=conn.getBandwidth();
+						remainingIp-=conn.getBandwidth();
 					}					
 				}
 			}
