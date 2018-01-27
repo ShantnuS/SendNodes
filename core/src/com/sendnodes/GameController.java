278f3c468dc5b608721a7d3fa7e6c69d25f882c1
hercu <hercu@10.9.168.176>
2018-01-27 17:38:35 +0000

    Created package for sound engine Added an instance of the SoundManager in GameController

--- core/src/com/sendnodes/GameController.java
+++ core/src/com/sendnodes/GameController.java
@@ -3,12 +3,16 @@ package com.sendnodes;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.sendnodes.entities.EntityManager;
 
+import soundengine.SoundManager;
+
 public class GameController {
 	
 	private EntityManager entityManager;
+	private SoundManager soundManager;
 	
 	public GameController(int map_size) {
 		entityManager = new EntityManager(map_size);
+		soundManager = new SoundManager();
 	}
 	
 	public void update() {
