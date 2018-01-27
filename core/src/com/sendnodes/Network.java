d44ef3468c228b5353251ad77a18a03cd4fa4fde
jt10g15 <jt10g15@m20.clients.soton.ac.uk>
2018-01-27 14:15:48 +0000

    Implemented isConnected. Added chance to connection generation.

--- core/src/com/sendnodes/Network.java
+++ core/src/com/sendnodes/Network.java
@@ -1,8 +1,10 @@
 package com.sendnodes;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
+import com.sendnodes.entities.Player;
 import com.sendnodes.nodes.Connection;
 import com.sendnodes.nodes.Node;
 
@@ -17,34 +19,47 @@ public class Network {
 		nodes = new ArrayList<Node>();
 		
 		Node[][] node_grid = new Node[size][size];
+		Connection conn;
 		
 		for (int x=0; x < size; x++) {
 			for (int y=0; y < size; y++) {
-				// for each default node that is not on the outside, generate adjacent connections.
+				// for each default node that is not on the outside, generate adjacent connections 66% of the time.
 				if (x > 1 && y > 1 && x < size-1 && y < size-1) {
-					Connection conn = new Connection(node_grid[x][y], node_grid[x+1][y], 1);
-					node_grid[x][y].addConnection(conn);
-					node_grid[x+1][y].addConnection(conn);
+					if (r.nextInt(3) != 1) {
+						conn = new Connection(node_grid[x][y], node_grid[x+1][y], 1);
+						node_grid[x][y].addConnection(conn);
+						node_grid[x+1][y].addConnection(conn);
+					}
 					
-					conn = new Connection(node_grid[x][y], node_grid[x][y+1], 1);
-					node_grid[x][y].addConnection(conn);
-					node_grid[x][y+1].addConnection(conn);
+					if (r.nextInt(3) != 1) {
+						conn = new Connection(node_grid[x][y], node_grid[x][y+1], 1);
+						node_grid[x][y].addConnection(conn);
+						node_grid[x][y+1].addConnection(conn);
+					}
 					
-					conn = new Connection(node_grid[x][y], node_grid[x+1][y+1], 1);
-					node_grid[x][y].addConnection(conn);
-					node_grid[x+1][y+1].addConnection(conn);
+					if (r.nextInt(3) != 1) {
+						conn = new Connection(node_grid[x][y], node_grid[x+1][y+1], 1);
+						node_grid[x][y].addConnection(conn);
+						node_grid[x+1][y+1].addConnection(conn);
+					}
 					
-					conn = new Connection(node_grid[x][y], node_grid[x-1][y], 1);
-					node_grid[x][y].addConnection(conn);
-					node_grid[x-1][y].addConnection(conn);
+					if (r.nextInt(3) != 1) {
+						conn = new Connection(node_grid[x][y], node_grid[x-1][y], 1);
+						node_grid[x][y].addConnection(conn);
+						node_grid[x-1][y].addConnection(conn);
+					}
 					
-					conn = new Connection(node_grid[x][y], node_grid[x][y-1], 1);
-					node_grid[x][y].addConnection(conn);
-					node_grid[x][y-1].addConnection(conn);
+					if (r.nextInt(3) != 1) {
+						conn = new Connection(node_grid[x][y], node_grid[x][y-1], 1);
+						node_grid[x][y].addConnection(conn);
+						node_grid[x][y-1].addConnection(conn);
+					}
 					
-					conn = new Connection(node_grid[x][y], node_grid[x-1][y-1], 1);
-					node_grid[x][y].addConnection(conn);
-					node_grid[x-1][y-1].addConnection(conn);
+					if (r.nextInt(3) != 1) {
+						conn = new Connection(node_grid[x][y], node_grid[x-1][y-1], 1);
+						node_grid[x][y].addConnection(conn);
+						node_grid[x-1][y-1].addConnection(conn);
+					}				
 				}
 				
 			}
@@ -54,16 +69,13 @@ public class Network {
 		for (int x=0; x < size; x++) {
 			for (int y=0; y < size; y++) {
 				if (r.nextInt(2) == 1) {
-					// 50% chance
-					for (Connection conn : node_grid[x][y].getConnections()) {
-						// remove connection from other node
-						conn.getOtherNode(node_grid[x][y]);
-					}
+					// 50% chance to remove a node and its connections.
+					for (Connection null_conn : node_grid[x][y].getConnections())
+						null_conn.getOtherNode(node_grid[x][y]).removeConnection(null_conn);
 					node_grid[x][y] = null;
 				}
 			}
 		}
-			
 		
 		// Empty the nodes generated into the ArrayList.
 		for (int x=0; x < size; x++) {
@@ -77,6 +89,35 @@ public class Network {
 	// use for starting node
 	public Node getRandomNode() {
 		return nodes.get(r.nextInt(nodes.size()));
+	}
+	
+	public boolean isConnected(Player player, Node node1, Node node2) {
+        List<Node> queue = new ArrayList<Node>();
+        
+        queue.add( node1 ); // start from here
+        boolean pathExists = false;
+        
+        while (!queue.isEmpty()) {
+            Node current = queue.remove(0);
+            
+            if (current == node2) {
+                pathExists = true;
+                break;
+            }
+            
+            for (Connection conn : current.getConnections()) {
+            	Node other = conn.getOtherNode(current);
+            	if (other.getOwner() == player)
+            		queue.add(conn.getOtherNode(current));
+            	else if (other == node2) {
+            		pathExists = true;
+                    break;
+            	}
+            		
+            }
+        }
+        
+        return pathExists;
 	}
 	
 	public int getMapSize() {
